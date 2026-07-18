161fe9e9279e28ab8e084a813e7cba47f249cd2c
magedmogeb9-hash <magedmogeb9@gmail.com>
2026-07-18 11:18:33 +0200

    Delete ApiKey.js

--- ApiKey.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const ApiKeySchema = new mongoose.Schema({
-    platform: String,
-    key: String,
-    active: Boolean
-});
-
-module.exports = mongoose.model("ApiKey", ApiKeySchema);
